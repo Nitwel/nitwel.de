d9e808a367bc4fa22471ded2d1cd9055187e90ce
Nitwel <mail@nitwel.de>
2023-10-16 00:35:46 +0200

    fix getImage again

--- util/image.ts
+++ util/image.ts
@@ -5,7 +5,7 @@ export type ImageOptions = Partial<{
 }>
 
 export function getImage(id: string, options: ImageOptions = {}) {
-    let url = new URL(`https://nitwel.de/assets/${id}`)
+    let url = new URL(`https://db.nitwel.de/assets/${id}`)
 
     for (let [key, val] of Object.entries(options)) {
         url.searchParams.set(key, val.toString())
